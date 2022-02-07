f6c23ff445958b9ad0b4cafb6f521c3876885e93
Shamunesh Selvam <64699355+SHAMUNESH@users.noreply.github.com>
2022-02-07 13:30:04 +0530

    files uploaded (7-2)

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter } from 'react-router-dom';
+import { Route } from 'react-router-dom/cjs/react-router-dom.min';
+import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import Cart from './components/Cart';
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <div>
+        <Route path='/' exact>
+          <Home />
+        </Route>
+        <Route path='/cart' exact>
+          <Cart />
+        </Route>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
